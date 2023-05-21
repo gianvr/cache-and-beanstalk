4faa0acb226c2b75a84627a3a0622a33ddb19c69
gianvr <gvgiancarlo1@gmail.com>
2023-05-21 20:35:46 -0300

    nomes melhorados

--- elastic_beanstalk.tf
+++ elastic_beanstalk.tf
@@ -1,5 +1,5 @@
 resource "aws_elastic_beanstalk_application" "application" {
-  name        = "my-awesome-app"
+  name        = "app-beanstalk"
   
 }
 resource "aws_iam_role" "beanstalk" {
@@ -13,7 +13,7 @@ resource "aws_iam_instance_profile" "beanstalk_instance_profile" {
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "my-awesome-environment"
+  name                = "beanstalk-env"
   application         = aws_elastic_beanstalk_application.application.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.0.0 running Python 3.11"
 
@@ -25,37 +25,29 @@ resource "aws_elastic_beanstalk_environment" "environment" {
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "REDIS_ENDPOINT"
-    value     = aws_elasticache_replication_group.example.primary_endpoint_address
+    value     = aws_elasticache_replication_group.cache_replication_group_beanstalk.primary_endpoint_address
   }
 }
 
-resource "aws_vpc" "foo" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "tf-test"
-  }
 }
 
-resource "aws_subnet" "foo" {
-  vpc_id            = aws_vpc.foo.id
+resource "aws_subnet" "subnet_cache" {
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "tf-test"
-  }
 }
 
-resource "aws_elasticache_subnet_group" "example" {
-  name       = "example"
-  subnet_ids = [aws_subnet.foo.id]
+resource "aws_elasticache_subnet_group" "subnet_group_cache" {
+  name       = "subnet-group-cache"
+  subnet_ids = [aws_subnet.subnet_cache.id]
 }
 
-resource "aws_elasticache_replication_group" "example" {
-  replication_group_id = "example"
-  description         = "teste"
+resource "aws_elasticache_replication_group" "cache_replication_group_beanstalk" {
+  replication_group_id = "cache-replication-group-beanstalk"
+  description          = "beanstalk-redis"
   node_type            = "cache.m4.large"
   port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.example.name
+  subnet_group_name    = aws_elasticache_subnet_group.subnet_group_cache.name
 }
